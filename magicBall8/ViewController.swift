a4bbf99af7d4e31fd0f0a6d0017dfa2741e3d6eb
Wedlec129 <boryasokolov@gmail.com>
2023-03-16 14:14:43 +0300

    Initial Commit

---
+++ magicBall8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  magicBall8
+//
+//  Created by Борух Соколов on 16.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
