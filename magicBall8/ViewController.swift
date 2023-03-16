db9cc293c138d3ccb6b1a7cca516e4a64a450a1e
Wedlec129 <boryasokolov@gmail.com>
2023-03-16 14:43:16 +0300

    у

--- magicBall8/ViewController.swift
+++ magicBall8/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //само изображение с ответом
+    @IBOutlet weak var ballOnScrean: UIImageView!
+    
+    var imageBall=[
+        UIImage(imageLiteralResourceName: "ball1"),
+        UIImage(imageLiteralResourceName: "ball2"),
+        UIImage(imageLiteralResourceName: "ball3"),
+        UIImage(imageLiteralResourceName: "ball4"),
+        UIImage(imageLiteralResourceName: "ball5"),
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    
+    
+    //нажатие на кнопку полусить овет
+    @IBAction func buttonGetAnsver(_ sender: Any) {
+        ballOnScrean.image=imageBall.randomElement()
+    }
+    
+    
+    
 }
 
